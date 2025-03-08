9b48c4f1f525e346961746b5d19643fdf7843d01
christophergyman <christophergayiuman@gmail.com>
2025-03-08 13:02:55 +0000

    feat: RESTAPI endpoint shell created for AccountController

---
+++ springboot-platform/TradingPlatform/src/main/java/org/ada/tradingplatform/LoginAccountController.java
@@ -0,0 +1,28 @@
+package org.ada.tradingplatform;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class LoginAccountController{
+
+    @PostMapping("/AccountPost")
+    public String AccountPost(){
+        return String.format("tester");
+    }
+
+    @GetMapping("/AccountLogin")
+    public String AccountLogin() {
+        return String.format("tester");
+    }
+
+    @DeleteMapping("/AccountDelete")
+    public String AccountDelete() {
+        return String.format("tester");
+    }
+
+    @PutMapping("/AccountEmailUpdate")
+    public String AccountEmailUpdate() {
+        return String.format("tester");
+    }
+}
