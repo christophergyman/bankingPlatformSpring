b4cb32e6154733370632300671b0341277c65538
christophergyman <christophergayiuman@gmail.com>
2025-03-08 12:52:14 +0000

    feat: UserBankAccount class created

---
+++ springboot-platform/TradingPlatform/src/main/java/org/ada/tradingplatform/UserBankAccount.java
@@ -0,0 +1,50 @@
+package org.ada.tradingplatform;
+import java.time.LocalDateTime;
+
+public class UserBankAccount {
+    private long bankID;
+    private long accountNumber;
+    private long routingNumber;
+    private int branchId;
+    private double balance;
+    private LocalDateTime openingDate;
+    private LocalDateTime closingDate;
+    private BalanceState balanceState;
+    private AccountTier accountTier;
+
+    public enum BalanceState{OPEN, CLOSED, PAUSED};
+    public enum AccountTier{TRIAL, BRONZE, GOLD};
+
+    // Admin Constructor
+    public UserBankAccount(long bankID, long accountNumber, long routingNumber, int branchId, double balance,
+                           LocalDateTime openingDate, LocalDateTime closingDate, BalanceState balanceState,
+                           AccountTier accountTier) {
+
+        this.bankID = bankID;
+        this.accountNumber = accountNumber;
+        this.routingNumber = routingNumber;
+        this.branchId = branchId;
+        this.balance = balance;
+        this.openingDate = openingDate;
+        this.closingDate = closingDate;
+        this.balanceState = balanceState;
+        this.accountTier = accountTier;
+
+    }
+    //setter
+    public void setBalance(double balance){ this.balance = balance; }
+    public void setAccountTier(AccountTier accountTier){ this.accountTier = accountTier; }
+
+    //getter
+
+    // Getters
+    public long getBankID() { return bankID; }
+    public long getAccountNumber() { return accountNumber; }
+    public long getRoutingNumber() { return routingNumber; }
+    public int getBranchId() { return branchId; }
+    public double getBalance() { return balance; }
+    public LocalDateTime getOpeningDate() { return openingDate; }
+    public LocalDateTime getClosingDate() { return closingDate; }
+    public BalanceState getBalanceState() { return balanceState; }
+    public AccountTier getAccountTier() { return accountTier; }
+}
